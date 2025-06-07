ae76d4a18c189d134683bfb324b3fe22b0e2bbff
Carolina Floro <carolinarochaf@hotmail.com>
2025-06-07 19:02:58 -0300

    Created account entity

---
+++ Domain/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Account
+{
+    public required string AccountNumber { get; set; }
+    public required string Name { get; set; }
+    public decimal Balance { get; set; } = 0m;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime LastUpdatedAt { get; set; }
+}
