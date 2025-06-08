8ddf2e6c6bbef03283899730019153cb7396338e
Carolina Floro <carolinarochaf@hotmail.com>
2025-06-08 17:19:14 -0300

    Criada classe Data

---
+++ Infra.Data/Data.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Events;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data;
+public class Data : IData
+{
+    public void CreateAccount(Account account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Account GetAccount(string account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<BaseEvent> GetAllEvents(string account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<BaseEvent> GetEventsSince(string account, DateOnly date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Snapshot GetSnapshot(DateOnly? date, string account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveEvent(BaseEvent evt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveSnapshot(Snapshot snapshot)
+    {
+        throw new NotImplementedException();
+    }
+}
