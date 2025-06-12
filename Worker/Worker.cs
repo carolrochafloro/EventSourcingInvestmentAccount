7300fed16378e462bc605c343c438b004dacbd28
Carolina Floro <carolinarochaf@hotmail.com>
2025-06-12 06:30:35 -0300

    implementado ws

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -1,22 +1,62 @@
+using Domain.Events;
+using Domain.Interfaces;
+
 namespace Worker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IQueue _queue;
+    private readonly IBusiness _business;
+    private readonly ICapitalContributionHandler _capitalContributionHandler;
+    private readonly IWithdrawalHandler _withdrawalHandler;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IQueue queue, IBusiness business, ICapitalContributionHandler capitalContributionHandler, IWithdrawalHandler withdrawalHandler)
     {
         _logger = logger;
+        _queue = queue;
+        _business = business;
+        _capitalContributionHandler = capitalContributionHandler;
+        _withdrawalHandler = withdrawalHandler;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            try
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                BaseEvent evt = await _queue.Consume();
+
+                _logger.LogInformation("Evento recebido: {EventName} - Conta: {Account}", evt.EventName, evt.Account);
+
+                switch (evt)
+                {
+                    case CapitalContribution cc:
+                        _capitalContributionHandler.HandleEvent(cc);
+                        break;
+
+                    case Withdrawal wd:
+                        _withdrawalHandler.HandleEvent(wd);
+                        break;
+
+                    case ReversalEvent reversal:
+                        _logger.LogInformation("Reversão recebida para o evento: {OriginalEventId}", reversal.OriginalEventId);
+                        _business.RollbackEvent(reversal);
+                        break;
+
+                    default:
+                        _logger.LogWarning("Evento não tratado: {EventName}", evt.EventName);
+                        break;
+                }
+
+                _business.CreateSnapshot(evt.Account);
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao processar evento");
             }
+
             await Task.Delay(1000, stoppingToken);
         }
     }
